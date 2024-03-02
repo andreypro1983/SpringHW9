5ffe082257ff76e3fb8d46be8ed47c12031b0ba9
Prozhikin_Andrey <a.prozhikin@gmail.com>
2024-03-02 19:56:11 +0200

    add eureka-server into repository

---
+++ SpringHW9EurekaServer/src/test/java/ru/geekbrains/SpringHW9EurekaServer/SpringHw9EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringHW9EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw9EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
